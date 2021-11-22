996981e8b8e2144811c8a9c5106f43131289f39a
Vishnu Vardhan <jaisrini76@gmail.com>
2021-11-23 01:27:35 +0530

    First Build

--- src/Chatbot/chat.js
+++ src/Chatbot/chat.js
@@ -1,12 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import {GiftedChat} from 'react-native-gifted-chat'
 
-const chat = () => {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+const botAvatar = require('../assets/Img/Botpic.png')
+const chatBot = {
+    _id: 2,
+    name: 'chatBot'
+    avatar: botAvatar
 }
 
-export default chat
+export default class chat extends Component {
+    state = {
+        messages: [
+            {_id: 1, text: 'Hello!', createdAt: new Date(), user: chatBot}
+        ],
+        id: 1,
+        name: ''
+    }
+
+
+    render() {
+        return (
+            <View style={styles.chatbg}>
+                <GiftedChat 
+                    messages={this.state.messages}
+                    user={[_id: 1]}
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    chatbg: {
+        flex: 1,
+        backgroundColor: '#fff'
+    }
+})
