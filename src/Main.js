e6b0a68f44ee60e4f885390bea417aaed26fdde4
Vishnu Vardhan <jaisrini76@gmail.com>
2022-04-03 01:23:16 +0530

    Tab Bar

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {Text, Image, Dimensions, SafeAreaView, StyleSheet, View} from 'react-native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+//Resources
 import Chat from './Screens/Chat'
 import Rating from './Screens/Rating'
 import About from './Screens/About'
@@ -10,23 +12,34 @@ const AboutIcon = require('../assets/Img/i.png')
 
 const Tab = createBottomTabNavigator()
 
-
 export default function Main() {
   return (
-      
-      <Tab.Navigator screenOptions={{tabBarShowLabel: false}}>
+    
+      <Tab.Navigator screenOptions={{
+          tabBarShowLabel: false, 
+          tabBarStyle: style.tabBar,
+          tabBarHideOnKeyboard: true,
+        }}>
+
         <Tab.Screen name='Chat' component={Chat} options={{
-            tabBarIcon: () => (<Image source={ChatBotIcon} style={style.AppIcon}/>)
+            tabBarIcon: () => (<Image source={ChatBotIcon} style={style.AppIcon}/>),
+            headerStyle: style.header,
+            tabBarStyle: {backgroundColor: '#f6e2bc', paddingHorizontal: 70}
         }}/>
         
 
         <Tab.Screen name='Rating' component={Rating} options={{
-            tabBarIcon: () => (<Image source={RatingIcon} style={style.AppIcon}/>)
+            tabBarIcon: () => (<Image source={RatingIcon} style={style.AppIcon}/>),
+            headerStyle: style.header,
+            tabBarStyle: [style.tabBar, {position: 'absolute'}]
+            
         }}/>
 
 
         <Tab.Screen name='About' component={About} options={{
-            tabBarIcon: () => (<Image source={AboutIcon} style={style.AppIcon}/>)
+            tabBarIcon: () => (<Image source={AboutIcon} style={style.AppIcon}/>),
+            headerStyle: style.header,
+            tabBarStyle: [style.tabBar, {position: 'absolute'}]
         }}/>
       </Tab.Navigator>
   )
@@ -36,5 +49,14 @@ const style = StyleSheet.create({
     AppIcon: {
         width: 20,
         height: 20,
+    },
+    tabBar: {
+        backgroundColor: '#f6e2bc',
+        borderRadius: 50,
+        marginHorizontal: 70,
+    },
+    header: {
+        backgroundColor: '#f6e2bc'
     }
+    
 })
--- src/Screens/Rating.js
+++ src/Screens/Rating.js
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Rating() {
   return (
-    <View>
+    <View style={style.bg}>
       <Text>This is Rating Screen</Text>
     </View>
   )
 }
+
+const style = StyleSheet.create({
+  bg: {
+    backgroundColor: '#f6e2bc',
+    flex: 1
+  }
+})
