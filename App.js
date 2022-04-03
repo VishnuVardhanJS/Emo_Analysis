eeb49e49f0b4567626a6161005638feb0c4e9282
Vishnu Vardhan <jaisrini76@gmail.com>
2022-04-03 21:32:35 +0530

    tab Bar

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import Chat from './src/Screens/Chat';
 const Stack = createStackNavigator();
 
 const App = () => {
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
